ebe64cc715bb8ead9d6af5e1f17b2acfff7e4e25
tharun_gade <tharungade2001@gmail.com>
2023-10-08 16:32:57 +0530

    implements calculateTime() function

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,12 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+  let startTime = new Date();
+  let sum = 0;
+  for (let i = 1; i <= n; ++i) {
+    sum += i;
+  }
+  
+  let endTime = new Date();
+  return endTime.getSeconds() - startTime.getSeconds();
 }
